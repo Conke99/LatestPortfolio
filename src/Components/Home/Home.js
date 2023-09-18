6d8740759898150f1023e02549d3434155bd12e2
Aleksandar Conic <acaconic99@gmail.com>
2023-09-18 03:42:57 +0200

    aditional changes in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,16 +5,19 @@ const Home = () => {
     <div name="Home" className="home">
       <div className="home_contect">
         <div className="right_side">
-          <h3>
-            Hello, I am <span>Aleksandar Conić</span>
-          </h3>
-          <h1>Fullstack Developer</h1>
-          <p>
-            I am a self-taught Fullstack Developer hailing from Serbia. My
-            passion lies in crafting captivating projects that seamlessly
-            integrate both frontend and backend development, allowing me to also
-            explore the world of design.
-          </p>
+          <div className="right_side-wrapper">
+            <h3>
+              Hello, I am <span>Aleksandar Conić</span>
+            </h3>
+
+            <h1>Fullstack Developer</h1>
+            <p>
+              I am a self-taught Fullstack Developer hailing from Serbia. My
+              passion lies in crafting captivating projects that seamlessly
+              integrate both frontend and backend development, allowing me to
+              also explore the world of design.
+            </p>
+          </div>
         </div>
         <div className="left_side">
           <img src="/Images/Profile.jpg" alt="Avatar" />
